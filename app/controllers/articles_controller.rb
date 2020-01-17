39b9a438e6f59c72f875f02d13cdd4f8ee224440
prashant <prashant@bajratechnologies.com>
2020-01-17 12:27:51 +0545

    Update current_user in article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspectS
     @article = Article.new(article_params)
-    @article.user = User.first    
+    @article.user = current_user   
     if @article.save
       flash[:success] = "Article was created successfully."
       redirect_to article_path(@article)
